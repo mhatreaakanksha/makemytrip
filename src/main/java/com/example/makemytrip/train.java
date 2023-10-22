3984dc2da97cc92dec0adc7892f5e0a5b74c7346
mhatreaakanksha <mhatreaakanksha97@gmail.com>
2023-10-22 18:43:55 +0530

    Adding new train module

--- src/main/java/com/example/makemytrip/train.java
+++ src/main/java/com/example/makemytrip/train.java
@@ -1,12 +1,14 @@
 package com.example.makemytrip;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
-public class hotel{
+public class train {
 
-    @GetMapping("/hotel")
-    public String getData() {return "please book your ticket from blue heaven" ;}
+    @GetMapping("/train")
+    public String getData() {return "Please book your ticket " ;}
 }
+
